d409620df5e75c02832cef06c81e01cb422f735a
deepa <deepakshettyksd@gmail.com>
2025-01-25 13:04:51 +0530

    topView function added

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -210,6 +210,45 @@ vector<int> verticalOrderTraversal(Node *root)
     return ans;
 }
 
+ vector<int> topView(Node *root) {
+       vector<int> ans;
+       
+       if(root==NULL){
+           return ans;
+       }
+       
+        map<int,int> map;
+       queue< pair<Node*,int> > q;
+       q.push(make_pair(root, 0));
+       
+      
+       
+       while(!q.empty()){
+           pair<Node* , int> temp = q.front();
+           q.pop();
+           Node* frontNode = temp.first;
+           int hd = temp.second;
+           
+           if(map.find(hd) == map.end()){
+               map[hd] = frontNode->data;
+           }
+           
+           if(frontNode->left){
+               q.push(make_pair(frontNode->left,hd-1));
+           }
+           
+           if(frontNode->right){
+               q.push(make_pair(frontNode->right,hd+1));
+           }
+       }
+       
+       for(auto i:map){
+           ans.push_back(i.second);
+       }
+       
+       return ans;
+    }
+
 
 int main() {
   Node *root = NULL;
