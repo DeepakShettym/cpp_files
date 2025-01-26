aa66e5fadda9b2930020390eb691ec95e1d2a966
deepa <deepakshettyksd@gmail.com>
2025-01-26 12:10:12 +0530

    sumOfLongRootToLeafPath function added

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -249,6 +249,113 @@ vector<int> verticalOrderTraversal(Node *root)
        return ans;
     }
 
+     vector <int> bottomView(Node *root) {
+      vector<int> ans;
+       
+       if(root==NULL){
+           return ans;
+       }
+       
+        map<int,int> map;
+       queue< pair<Node*,int> > q;
+       q.push(make_pair(root, 0));
+       
+      
+       
+       while(!q.empty()){
+           pair<Node* , int> temp = q.front();
+           q.pop();
+           Node* frontNode = temp.first;
+           int hd = temp.second;
+           
+               map[hd] = frontNode->data;
+          
+           
+           if(frontNode->left){
+               q.push(make_pair(frontNode->left,hd-1));
+           }
+           
+           if(frontNode->right){
+               q.push(make_pair(frontNode->right,hd+1));
+           }
+       }
+       
+       for(auto i:map){
+           ans.push_back(i.second);
+       }
+       
+       return ans;
+    }
+
+  vector<int> leftView(Node *root) {
+       vector<int> ans;
+       solve(root,ans,0);
+       return ans;
+    }  
+void solve(Node* root,vector<int> &ans,int level){
+        if(root==NULL){
+            return;
+        }
+        if(level==ans.size()){
+            ans.push_back(root->data);
+        }
+        solve(root->left,ans,level+1);
+        
+        solve(root->right,ans,level+1);
+    }
+
+
+ vector<int> rightView(Node *root)
+    {
+        vector<int> ans;
+       solve(root,ans,0);
+       return ans;
+    }
+void solve(Node* root,vector<int> &ans,int level){
+        if(root==NULL){
+            return;
+        }
+        if(level==ans.size()){
+            ans.push_back(root->data);
+        }
+        
+          solve(root->right,ans,level+1);
+        solve(root->left,ans,level+1);
+        
+      
+    }
+
+        void solve(Node* root,int len,int &maxLen,int sum,int &maxSum){
+        if(root==NULL){
+            if(len > maxLen){
+                maxLen = len;
+                maxSum = sum;
+            }else if(len == maxLen){
+                maxSum = max(sum,maxSum);
+            }
+            
+            return;
+            
+        }
+            
+            sum = sum + root->data;
+            
+            solve(root->left,len+1,maxLen,sum,maxSum);
+            solve(root->right,len+1,maxLen,sum,maxSum);
+        
+    }
+    int sumOfLongRootToLeafPath(Node *root) {
+        int len = 0;
+        int maxLen = 0;
+        int sum = 0;
+        int maxSum = INT_MIN;
+
+    solve(root, len, maxLen, sum, maxSum);
+
+    return maxSum;
+    }
+  
+ 
 
 int main() {
   Node *root = NULL;
