f8541c3590b94397005d1004b88e4fcf676f28af
deepa <deepakshettyksd@gmail.com>
2025-02-17 10:30:07 +0530

    .\setBit.cpp added

---
+++ bit_Manipulation/setBit.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+bool isSetBit(int num, int i) {
+    return (num & (1 << i));
+}
+
+int main() {
+    int i = 2;
+    int num = 13;
+
+    bool ans = isSetBit(num, i);
+
+    cout << ans << endl;
+
+    return 0;
+}
