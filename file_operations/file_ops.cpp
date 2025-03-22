d221757984a115a114d591ca6cd7796e7ffb0cec
deepa <deepakshettyksd@gmail.com>
2025-03-22 22:43:23 +0530

    file operation added

---
+++ file_operations/file_ops.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+int main(){
+
+  string arr[5] = {"hey how are u xyz","hey how are u abc" ,"hey how are u pqr","hey how are u qwe","hey how are u mnb"};
+
+  ofstream outFile("textFile.txt");
+
+  if(!outFile){
+    cout << "cannot create file" << endl;
+  }else{
+    cout << "file created sucessfully" << endl;
+  }
+
+
+  for(int i = 0;i<5;i++){
+    outFile << arr[i] << endl;
+  }
+
+  
+  outFile.close();
+
+  ifstream inFile("textFile.txt");
+
+  if(!inFile){
+    cout << "file does not exist" << endl;
+  }else{
+    cout << "file is present";
+  }
+
+  string line;
+
+  int targetLine = 3; // read 3rd line
+  int startLine = 1;
+  cout << "\nReading from the file:" << endl;
+  while (getline(inFile, line)) {
+      if(startLine == targetLine){
+        cout << line << endl;
+        break;
+      } 
+      startLine++; 
+  }
+
+  inFile.close();
+  
+  ifstream inFile1("textFile.txt");  
+
+
+  int stopLine = 3; // read until 3rd line
+  startLine = 0;
+
+  while (getline(inFile1, line)) {
+    if(startLine == stopLine){
+      break;
+    } 
+    cout << line << endl;
+    startLine++; 
+}
+
+ 
+
+  
+
+  return 0;
+}
