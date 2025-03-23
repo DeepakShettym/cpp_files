dcf62a40585b51f7fd4378e9f4116adfc188804d
deepa <deepakshettyksd@gmail.com>
2025-03-23 15:44:32 +0530

    dynamic_memory_allocation

---
+++ dynamic_memory_allocation/malloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr, *start;
+
+    ptr = (int *)malloc(5 * sizeof(int));
+    if (ptr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    start = ptr;  
+
+   
+    for (int i = 0; i < 5; i++) {
+        *ptr = i;
+        ptr = ptr + 1;
+    }
+
+    ptr = start;  
+
+   
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", *ptr);
+        ptr = ptr + 1;  
+    }
+
+    free(start);  
+
+    return 0;
+}
