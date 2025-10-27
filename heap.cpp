f0021434a0a2180555a7052c4ea8388706a403fe
deepa <deepakshettyksd@gmail.com>
2025-10-27 12:47:34 +0530

    mergeKLists added

--- heap.cpp
+++ heap.cpp
@@ -82,6 +82,14 @@ public:
             j = col;
         }
     };
+
+      struct ListNode {
+        int val;
+            ListNode *next;
+            ListNode() : val(0), next(nullptr) {}
+            ListNode(int x) : val(x), next(nullptr) {}
+            ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
     
     
     class compare{
@@ -164,6 +172,43 @@ void heapify(int arr[], int n, int i) {
         return ans;
         
         
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode* , vector<ListNode*> , compare> minHeap;
+
+        for(int i = 0 ; i < lists.size(); i++){
+            if(lists[i]!=NULL){
+                 minHeap.push(lists[i]);
+            }
+           
+        }
+
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        while(!minHeap.empty()){
+            ListNode* top = minHeap.top();
+
+            minHeap.pop();
+
+            if(top->next != NULL){
+                minHeap.push(top->next);
+            }
+
+            if(head == NULL){
+                head = top;
+                tail = top;
+            }else{
+                tail->next = top;
+                tail = top;
+            }
+
+
+        }
+
+
+        return head;
     }
 
 int main() {
