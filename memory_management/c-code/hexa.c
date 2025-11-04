1a7ff449f45f70f92ab1e81c5e3aca8b6bfe521d
deepa <deepakshettyksd@gmail.com>
2025-11-04 12:22:58 +0530

    hexa value conversion for types

---
+++ memory_management/c-code/hexa.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a = 0x22334455;
+
+    char* p =  (char *)&a;
+
+    *(p + 1)  = 0x88; // pinter moves 1 byte ahead ie 44 -> 88
+
+    printf("%x \n", a);
+
+    for(int i = 0 ; i < 4 ; i++){
+        printf("%x ", (unsigned char)*(p + i));
+    }
+
+
+    long long b = 0x9988776655;
+
+
+    int* p1 = (int*) &b;
+
+    *(p1 + 1) = 0x25;  // pinter moves 4 byte ahead ie 99 -> 25 
+
+     printf("%x \n", b);
+
+      char* p2 = (char*) &b;
+
+       for(int i = 0 ; i < 8 ; i++){
+        printf("%x ", (unsigned char)*(p2 + i)); // printing each  byte address value  
+     }
+
+     /* sample output : 
+        88776655 
+        55 66 77 88 25 0 0 0 
+     */
+
+     
+
+
+
+
+    
+}
