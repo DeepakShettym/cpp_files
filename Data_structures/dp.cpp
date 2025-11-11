18b2ab2a7264d76da6097e9edc2080cc06be58e4
DeepakShettym <deepakshettyksd@gmail.com>
2025-11-11 15:40:54 +0530

    mazeObstacles implimentation added

--- Data_structures/dp.cpp
+++ Data_structures/dp.cpp
@@ -118,6 +118,34 @@ int uniquePaths(int m, int n) {
 
 
 
+const int MOD = 1e9 + 7;
+
+int mazeObstacles(int n, int m, vector<vector<int>> &mat) {
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (mat[i][j] == -1) {
+                dp[i][j] = 0; 
+            
+            }else if(i == 0 && j == 0) {
+                dp[i][j] = 1; 
+
+            }else {
+            int up = 0, left = 0;
+            if (i > 0) up = dp[i - 1][j];
+            if (j > 0) left = dp[i][j - 1];
+
+            dp[i][j] = (up + left) % MOD;
+        }
+    }
+
+    return dp[n - 1][m - 1];
+}
+
+
+
+
 };
 
 
@@ -135,4 +163,6 @@ int main(){
 
     /* ans 1 : 8
     ans 2 : 8 */
+
+    return 0;
 }
