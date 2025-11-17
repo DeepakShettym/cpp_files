10341a0c088bac2e77d82f6b1db9b15bceb0d3df
DeepakShettym <deepakshettyksd@gmail.com>
2025-11-17 11:29:44 +0530

    minPathSum implimentaion added

--- Data_structures/dp.cpp
+++ Data_structures/dp.cpp
@@ -143,6 +143,33 @@ int mazeObstacles(int n, int m, vector<vector<int>> &mat) {
     return dp[n - 1][m - 1];
 }
 
+}
+
+    int minPathSum(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n , vector<int>(m ,  -1));
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }else{
+                    int down = INT_MAX;
+                    int right = INT_MAX;
+
+                    if(i > 0) down = grid[i][j] + dp[i-1][j];
+                    if(j > 0) right = grid[i][j] + dp[i][j-1];
+
+                    dp[i][j] = min(down , right);
+                }
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+
 
 
 
