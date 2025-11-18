56dda173180905fa2e2eb4683e0558eba1e397aa
DeepakShettym <deepakshettyksd@gmail.com>
2025-11-18 15:00:42 +0530

    added cherryPickup

--- Data_structures/dp.cpp
+++ Data_structures/dp.cpp
@@ -170,7 +170,7 @@ int mazeObstacles(int n, int m, vector<vector<int>> &mat) {
         return dp[n-1][m-1];
     }
 
-    int minimumPathSum(vector<vector<int>>& triangle, int n){
+    int minimumPathSumTriangle(vector<vector<int>>& triangle, int n){
 	
 
 	vector<vector<int>> dp(n , vector<int>(n , -1));
@@ -207,6 +207,55 @@ int mazeObstacles(int n, int m, vector<vector<int>> &mat) {
 
 		return  mini;
 }
+
+
+int solvecherryPickup(int i , int j1 ,int j2 , int r ,int c, vector<vector<int>>& grid , vector<vector<vector<int>>>& dp){
+        if(j1 < 0 || j1 > c - 1 || j2 < 0 || j2 > c - 1){
+            return -1e8;
+        }
+
+        if(i == r-1){
+            if(j1 == j2 ){
+                return grid[i][j1];
+            }else{
+                return grid[i][j1] + grid[i][j2];
+            }
+        }
+
+        if(dp[i][j1][j2] != -1){
+            return dp[i][j1][j2];
+        }
+        
+
+        int maxi = -1e8;
+
+
+        for(int dj1 = -1 ; dj1 <= 1 ; dj1++){
+            for(int dj2 = -1 ; dj2 <= 1 ; dj2++){
+                if(j1 == j2){
+                     maxi =  max(maxi , grid[i][j1] + solvecherryPickup(i + 1 , j1 + dj1 , j2 + dj2 , r , c, grid,dp));
+                }else{
+                     maxi =  max(maxi , grid[i][j1] + grid[i][j2]  + solvecherryPickup(i + 1 , j1 + dj1 , j2 + dj2 ,r,c ,grid,dp) );
+
+                }
+            }
+        }
+
+
+        return dp[i][j1][j2] =  maxi ;
+
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        int r = grid.size();
+        int c = grid[0].size();
+
+    vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
+
+        return solvecherryPickup(0,0,c-1,r,c,grid,dp);
+    }
+
+
     
 
 
