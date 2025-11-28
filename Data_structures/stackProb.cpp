525cafcf155963e5b18fc21a31330e65fb55d427
DeepakShettym <deepakshettyksd@gmail.com>
2025-11-28 11:35:55 +0530

    prefixToInfixConversion

--- Data_structures/stackProb.cpp
+++ Data_structures/stackProb.cpp
@@ -209,6 +209,31 @@ return ans;
 }
 
 
+string prefixToInfixConversion(string &s){
+	stack<string> st;
+
+	int i = s.length()-1;
+
+	while(i >= 0){
+		if(isalnum(s[i])) st.push(string(1,s[i]));
+
+		else {
+			string t1 = st.top() ; st.pop();
+			string t2 = st.top() ; st.pop();
+
+			string newString = "(" + t1 + s[i] + t2 + ")";
+
+			st.push(newString);
+		}
+
+
+		i--;
+	}
+
+	return st.top();
+}
+
+
    
 };
 
