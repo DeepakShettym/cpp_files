b50a1be9c7a94ba528d7a1cfc752989a7cde27f7
DeepakShettym <deepakshettyksd@gmail.com>
2025-11-29 09:42:18 +0530

    nextGreaterElement

--- Data_structures/stackProb.cpp
+++ Data_structures/stackProb.cpp
@@ -281,6 +281,22 @@ string preToPost(string s) {
    return st.top();
 }
 
+vector<int> nextGreaterElement(vector<int>& arr, int n)
+{
+	stack<int> st;
+	vector<int> ans(n , -1);
+	for(int i = 0 ; i < n ; i++){
+		while(!st.empty() && arr[i] > arr[st.top()]){
+			ans[st.top()] = arr[i];
+			st.pop();
+		}
+
+		st.push(i);
+	}
+
+	return ans;
+}
+
 
    
 };
