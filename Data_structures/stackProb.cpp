069cf156ba2d6ba4efcf183f33f73139342fd9a4
DeepakShettym <deepakshettyksd@gmail.com>
2025-12-01 11:25:15 +0530

    nextSmallerEle

--- Data_structures/stackProb.cpp
+++ Data_structures/stackProb.cpp
@@ -354,6 +354,35 @@ vector<int> prevSmaller(vector<int>& arr) {
         return ans;
     }
 
+
+      vector<int> nextSmallerEle(vector<int>& arr) {
+        //  code here
+        int n = arr.size();
+        
+        vector<int> ans(n);
+        
+        stack<int> st;
+        
+        for(int i = n-1 ; i>= 0 ; i--){
+            
+            while(!st.empty() && arr[i] <= st.top()) {
+                st.pop();
+            }
+            
+            
+            if(st.empty()){
+                ans[i] = -1;
+            }else{
+                ans[i] = st.top();
+            }
+            
+            
+            st.push(arr[i]);
+        }
+        
+        return ans;
+    }
+
    
 };
 
