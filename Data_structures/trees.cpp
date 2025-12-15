af6043adcdc7fcad00400b295ad650b36a43177f
DeepakShettym <deepakshettyksd@gmail.com>
2025-12-15 11:18:25 +0530

    getLeftView

--- Data_structures/trees.cpp
+++ Data_structures/trees.cpp
@@ -393,3 +393,33 @@ vector<int> bottomView(TreeNode<int> * root){
 
         return ans;
 }
+
+vector<int> getLeftView(TreeNode<int> *root)
+{   
+        vector<int> ans;
+
+        if(root == NULL){
+            return ans;
+        }
+
+        queue<TreeNode<int>*> q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+
+            for(int i = 0 ; i < size ; i++){
+                TreeNode<int>* node = q.front();
+                q.pop();
+
+                if(i == 0){
+                    ans.push_back(node->data);
+                }
+
+                if(node->left) q.push(node->left);
+
+                if(node->right) q.push(node->right);
+            }
+        }
+
+        return ans;
+}
